f29f8f7a2bb92be39ea480fbf3f0461a29a68b0e
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-01-16 20:20:57 +0100

    add arraay object literal bike

---
+++ script.js
@@ -0,0 +1,23 @@
+const bici = [
+    {
+        nome: Pinarello, Dogma,
+        peso: 25,
+    },
+    {
+        nome: Colnago,
+        peso: 32,
+    },
+     {
+        nome: Specialaized,
+        peso: 27,
+    },
+    {
+        nome: Trek,
+        peso: 35,
+    },
+     {
+        nome: Cannodale,
+        peso: 29,
+    },
+]
+
